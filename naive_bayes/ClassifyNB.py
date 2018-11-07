066a1d0d099adaa0bc7f1c55f18b51e3890ad00d
Héricles Emanuel <hericles.me@gmail.com>
2018-11-07 00:17:48 -0200

    naive_bayes example

---
+++ naive_bayes/ClassifyNB.py
@@ -0,0 +1,11 @@
+def classify(features_train, labels_train):   
+    ### import the sklearn module for GaussianNB
+    from sklearn.naive_bayes import GaussianNB
+    
+    ### create classifier
+    clf = GaussianNB()
+    
+    ### fit the classifier on the training features and labels
+    ### return the fit classifier
+    return clf.fit(features_train, labels_train)
+    
